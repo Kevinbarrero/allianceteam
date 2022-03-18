258dbd35a71f15c572420b0dca40e6e4dd57e26d
Kevinbarrero <Kevinbar98@gmail.com>
2022-03-18 21:39:40 +0300

    added math functions

---
+++ math_functions.py
@@ -0,0 +1,28 @@
+def mm_kor(socomo_props, a_value, kdsi, b_value):
+    result = 1
+    for x in socomo_props:
+        result *= x
+    result = (result*a_value) * (kdsi**b_value)
+    return result
+
+list_of_integers = [1.15, 1.06, 1.13, 1.17]
+## example mm_kor
+mmkor = mm_kor(list_of_integers, 3.2, 3.0, 1.05)
+print()
+##
+
+
+
+def t_dev(mm_kor_result, c_value):
+    STANDARD = 2.5
+    return STANDARD*mm_kor_result**c_value
+##example t_dev
+tdev = t_dev(mmkor, 0.38)
+print(tdev)
+
+
+
+##example team members
+def team_members(mmkor, tdev):
+    return mmkor / tdev
+print (team_members(mmkor,tdev))
--- start.py
+++ start.py
@@ -1,5 +1,33 @@
-import cocomoco
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QPushButton
 
-result = cocomoco.calculate(100000).dtime
-print(result)
+class Example(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+                
+        combo = QComboBox(self)
+        combo.addItem("Apple")
+        combo.addItem("Pear")
+        combo.addItem("Lemon")
+
+        combo.move(50, 50)
+
+        self.qlabel = QLabel(self)
+        self.qlabel.move(50,16)
+
+        combo.activated[str].connect(self.onChanged)      
+
+        self.setGeometry(50,50,320,200)
+        self.setWindowTitle("QLineEdit Example")
+        self.show()
+
+    def onChanged(self, text):
+        self.qlabel.setText(text)
+        self.qlabel.adjustSize()
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
 
